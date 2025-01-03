161311f46bc986dc5524212c9edd55c692195ee6
aadishiv <aadishiv@outlook.com>
2025-01-03 00:31:40 -0500

    first commit

---
+++ Imoji/ImojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImojiApp.swift
+//  Imoji
+//
+//  Created by Aadi Shiv Malhotra on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ImojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
